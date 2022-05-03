9d1142fadefcf019f919313625219efc8a9d2553
Marula <marula@Jeremias-Pfluger.local>
2022-05-03 16:00:11 +0200

    decathlon bike buyer

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import argparse
 import time
 
 from selenium import webdriver
+from selenium.webdriver import ChromeOptions
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
@@ -14,59 +14,44 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--size', type=str, help='Tamaño de la bicicleta que desea comprar. Ej: "L - 175-184CM"')
 parser.add_argument('--email', type=str, help='El mail con el cual se va loguear en su cuenta')
 parser.add_argument('--password', type=str, help='El password de su cuenta')
-parser.add_argument('--ciudad', type=str, help='Nombre de la ciudad en la cual desea obtener una cita. Ej: "MALLORCA"')
-parser.add_argument('--tipo_cita', type=str, help='El tipo de Cita que desea obtener. Ej: "INFORMACION GENERAL - IBIZA", "POLICIA-CERTIFICADOS Y ASIGNACION NIE"')
-parser.add_argument('--tipo_documento', type=str, choices=['NIE', 'DNI', 'PASAPORTE'], help='El tipo de documento con el que realizara el pedido de cita. NIE, DNI o PASAPORTE')
-parser.add_argument('--documento', type=str, help='El numero de documento en cuestion con el que realizara el tramite')
-parser.add_argument('--nacimiento', type=str, help='El año de su nacimiento en formato de 4 números. Ej: 1987, 1993')
-parser.add_argument('--nacionalidad', type=str, help='El país de nacionalidad de su documento')
-parser.add_argument('--nombre', type=str, help='El nombre completo de la persona que va realizar el tramite')
-parser.add_argument('--telefono', type=str, help='Su numero de telefono. Solo se aceptan telefonos españoles. NO anteponga el prefijo "+34"')
-parser.add_argument('--elegir_cita_automaticamente', type=str, choices=['SI', 'NO'], help='En caso que elija "SI" el sistema automáticamente le elegira la primer cita disponible. En caso de elegir "NO" usted podrá elegir la cita de su mayor conveniencia')
+parser.add_argument('--region', type=str, help='La region donde desear retirar su producto. Ej: "Comunidad de Madrid", "Euskadi"')
+parser.add_argument('--city', type=str, help='La ciudad donde retirará su producto. Ej: "City Vitoria (centro ciudad)"')
+parser.add_argument('--card_number', type=str, help='Numero de la tarjeta con la que va pagar')
+parser.add_argument('--card_expiration_month', type=str, help='El mes de expiracion de la tarjeta. Ej: "02", "07", "10"')
+parser.add_argument('--card_expiration_year', type=str, help='El año de expiracion de la tarjeta. Ej: "2022", "2026", "2030"')
+parser.add_argument('--card_secure_code', type=str, help='El codigo de seguridad de la tarjeta (tiene 3 digitos')
+
 args = parser.parse_args()
 
 BIKE_SIZE = args.size
 EMAIL = args.email
 PASSWORD = args.password
-CITY = args.ciudad
-APPOINTMENT_TYPE = args.tipo_cita
-DOCUMENT_TYPE = args.tipo_documento
-DOCUMENT = args.documento
-BIRTH_YEAR = args.nacimiento
-NATIONALITY = args.nacionalidad
-NAME = args.nombre
-PHONE = args.telefono
-POLICE_APPOINTMENTS = {'POLICIA', 'POLICÍA', 'CERTIFICADO', 'AUTORIZACIÓN', 'ASILO'}
+REGION = args.region
+CITY = args.city
+CARD_NUMBER = args.card_number
+CARD_EXPIRATION_MONTH = args.card_expiration_month
+CARD_EXPIRATION_YEAR = args.card_expiration_year
+CARD_SECURE_CODE = args.card_secure_code
 
-driver = webdriver.Chrome()
-appointment_available = False
+def start_browser():
+    options = ChromeOptions()
+    options.binary_location = "/Applications/Chromium.app/Contents/MacOS/Chromium"
+    options.add_argument('hide-scrollbars')
+    options.add_argument('disable-gpu')
+    options.add_argument('no-sandbox')
+    options.add_argument('disable-infobars')
+    options.add_argument('disable-web-security')
 
-
-def get_appointment_type_id(appointment_type):
-    if appointment_type.split()[0].split('-')[0].upper() in POLICE_APPOINTMENTS:
-        return 'tramiteGrupo[1]'
-
-    return 'tramiteGrupo[0]'
-
-
-def print_choosing_appointment_message():
-    print('NO CIERRE EL PROGRAMA AÚN!')
-    print('Debe seleccionar en este punto la cita deseada y luego completar con los datos faltantes')
-    print('Una vez que tenga confirmada su cita ya podrá cerrar el programa')
-
-
-def get_city_text(city_options):
-    for city in city_options:
-        if CITY.upper() in city:
-            return city
-    return ''
+    return webdriver.Chrome(chrome_options=options)
 
 
 while True:
     try:
+        driver = start_browser()
         driver.maximize_window()
-        # driver.get('https://www.decathlon.es/es/p/bicicleta-montana-allmountain-am-100-hardtail/_/R-p-331946')
-        driver.get("https://www.decathlon.es/es/p/bicicleta-electrica-de-montana-rockrider-ebike-st-100-27-5-azul/_/R-p-309736?mc=8560739")
+        driver.get('https://www.decathlon.es/es/p/bicicleta-montana-allmountain-am-100-hardtail/_/R-p-331946')
+        # driver.get("https://www.decathlon.es/es/p/bicicleta-electrica-de-montana-rockrider-ebike-st-100-27-5-azul/_/R-p-309736?mc=8560739")
+        # driver.get("https://www.decathlon.es/es/p/bicicleta-de-montana-rockrider-st-120-aluminio-monoplato-9v-27-5/_/R-p-305496")
         # Waiting for login page to be fully loaded
         try:
             WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, 'didomi-continue-without-agreeing')))
@@ -77,15 +62,10 @@ while True:
             WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'app')))
 
         driver.execute_script('window.scrollTo(0, 100);')
-        actions = ActionChains(driver)
-        size_selector = driver.find_element(By.ID, 'product-size-selection')
-        actions.move_to_element(size_selector).perform()
-        size_selector = Select(size_selector)
-        size_selector.select_by_visible_text(BIKE_SIZE)
-        # driver.execute_script("arguments[0].scrollIntoView();", )
-        # size_selector = driver.find_element(By.XPATH, './/div[@class="select svelte-1gsxlqt"]/button')
-        # size_selector.click()
-        driver.find_element(By.ID, 'option-product-size-selection-2').click()
+        size_selector = driver.find_element(By.XPATH, './/span[text()="Elegir talla"]')
+        driver.execute_script("arguments[0].click();", size_selector)
+        select_size = driver.find_element(By.XPATH, './/span[contains(text(),"{}")]'.format(BIKE_SIZE))
+        driver.execute_script("arguments[0].click();", select_size)
 
         # Check availability
         try:
@@ -97,12 +77,12 @@ while True:
         except NoSuchElementException:
             pass
         try:
-            driver.find_element(By.XPATH, './/button[@class="cta cta--block cta-noicon"]')
+            driver.find_element(By.XPATH, './/button[text()="Añadir a la cesta"]')
         except NoSuchElementException:
             print('Unable to locate "Add to cart" button')
             raise Exception('Unable to locate "Add to cart" button')
 
-        add_to_cart_button = driver.find_element(By.XPATH, './/button[@class="cta cta--block cta-noicon"]')
+        add_to_cart_button = driver.find_element(By.XPATH, './/button[text()="Añadir a la cesta"]')
         driver.execute_script("arguments[0].click();", add_to_cart_button)
         time.sleep(5)
 
@@ -111,7 +91,7 @@ while True:
         WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'mounting-component')))
 
         # Click buy
-        accept_button = driver.find_element(By.XPATH, './/button[@class="vtmn-btn vtmn-btn_variant--conversion vtmn-btn_size--medium vtmn-btn_size--stretched"]')
+        accept_button = driver.find_element(By.XPATH, './/button[text()="Comenzar pedido"]')
         driver.execute_script("arguments[0].click();", accept_button)
 
         # Wait for email element to be loaded
@@ -130,25 +110,56 @@ while True:
         password_input.send_keys(Keys.ENTER)
         time.sleep(5)
 
-        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, './/h1[@class="svelte-19f8svx"]')))
+        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, './/h2[text()="Recogida en tienda"]')))
 
-        store_pick_up_menu = driver.find_element(By.XPATH, './/div[@class="drawer-button svelte-bjp9q1"]')
-        store_pick_up_menu.click()
+        store_pick_up_menu = driver.find_element(By.XPATH, './/h2[text()="Recogida en tienda"]')
+        driver.execute_script("arguments[0].click();", store_pick_up_menu)
 
-        euskadi_select = driver.find_element(By.ID, 'option-9fb29edb-a486-43b5-a8ca-2622175698fd-11')
-        euskadi_select.click()
+        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, './/span[text()="Comunidad de Madrid"]')))
+        regions_selector = driver.find_element(By.XPATH, './/span[text()="Comunidad de Madrid"]')
+        driver.execute_script("arguments[0].click();", regions_selector)
+        time.sleep(5)
+        region_select = driver.find_element(By.XPATH, './/li[text()="{}"]'.format(REGION))
+        driver.execute_script("arguments[0].click();", region_select)
 
-        driver.find_element(By.XPATH, './/label[@for="0070188601886"]').click()
+        time.sleep(5)
+        city_selector = driver.find_element(By.XPATH, './/div[text()="{}"]'.format(CITY))
+        driver.execute_script("arguments[0].click();", city_selector)
 
-        pay_button = driver.find_element(By.XPATH, './/button[@class="vtmn-btn vtmn-btn_variant--conversion vtmn-btn_size--medium vtmn-btn_size--stretched"]')
-        pay_button.click()
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        continue_to_payment_button = driver.find_element(By.XPATH, './/button[text()="Continuar con el pago"]')
+        continue_to_payment_button.click()
 
-        availability_selector = driver.find_element()
+        time.sleep(5)
+        driver.refresh()
+        time.sleep(5)
+        pay_button = driver.find_element(By.XPATH, './/button[text()="Realizar pago"]')
+        driver.execute_script("arguments[0].click();", pay_button)
 
+        # Insert card data
+        time.sleep(5)
+        card_number_input = driver.find_element(By.ID, 'hidCardNumber')
+        card_number_input.send_keys(CARD_NUMBER)
+
+        expiry_month_selector = Select(driver.find_element(By.ID, 'hidMonth'))
+        expiry_month_selector.select_by_visible_text(CARD_EXPIRATION_MONTH)
+        expiry_year_selector = Select(driver.find_element(By.ID, 'hidYear'))
+        expiry_year_selector.select_by_visible_text(CARD_EXPIRATION_YEAR)
+
+        cvv_input = driver.find_element(By.ID, 'hidCvv')
+        cvv_input.send_keys(CARD_SECURE_CODE)
+
+        accept_button = driver.find_element(By.ID, 'hidAccept')
+        driver.execute_script("arguments[0].click();", accept_button)
+
+        print("Hemos llegado al final del proceso. En este momento le debería llegar una notificación de que la compra "
+              "ha sido exitosa")
+        print("Por favor confirme desde la aplicación de N26 y luego que la compra se ha completado exitosamente desde "
+              "su cuenta Decathlon")
+        end = input("Luego de la confirmación de esto, puede usted cerrar este programa. Muchas gracias")
     except Exception as ex:
         print(ex.__str__())
         driver.close()
         driver.quit()
         if ex.__str__() == 'No availability':
             time.sleep(60)
-        driver = webdriver.Chrome()
